dedc809b1340dea0579b61407dd54df0a7100b56
nathan ormond <nath.m.ormond@gmail.com>
2025-01-25 16:37:52 +0000

    init

---
+++ lectures/week1/misc.R
@@ -0,0 +1,31 @@
+# This is a single line comment
+x <- 5 # Comments can also go after code
+
+'This is a multi-line comment
+It can span several lines
+But its less commonly used than # comments'
+
+# Numeric Type
+a <- 1
+# This shows you the type 
+class(a)
+
+# Integer Type
+b <- 1L
+class(b)
+
+# Strings 
+fruit <- "Apple"
+fruit
+# You can use either single or double quotes to declare a string
+colour <- 'red'
+colour
+
+# Logical Types
+# Create an object called x which takes the value TRUE
+x <- TRUE
+
+# print the contents of the object x
+class(x)
+
+
